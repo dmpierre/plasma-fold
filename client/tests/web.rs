c555c696c2e95399ba4a16b51bec3cbb331098a2
winderica <winderica@gmail.com>
2025-05-10 00:13:40 +0800

    Attempt to use alias for client tests

--- client/tests/web.rs
+++ client/tests/web.rs
@@ -1,14 +1,7 @@
-//! Test suite for the Web and headless browsers.
-
-#![cfg(target_arch = "wasm32")]
-
-extern crate wasm_bindgen_test;
 use plasma_fold::datastructures::keypair::KeyPair;
 use wasm_bindgen_test::*;
 
-wasm_bindgen_test_configure!(run_in_browser);
-
 #[wasm_bindgen_test]
 fn test_keypair() {
-    let run_test = some_test();
+    assert_eq!(1 + 1, 2);
 }
