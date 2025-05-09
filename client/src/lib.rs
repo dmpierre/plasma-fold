8d24d52de36a93442b35422a6f41d1543befa0f5
dmpierre <pdaixmoreux@gmail.com>
2025-05-09 17:50:24 +0200

    example: keypair

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,10 +1,11 @@
 mod utils;
 
+use ark_grumpkin::Projective;
 use plasma_fold::datastructures::keypair::KeyPair;
 use wasm_bindgen::prelude::*;
 
 pub fn some_test() {
-    let keypair = KeyPair {
+    let keypair: KeyPair<Projective> = KeyPair {
         sk: todo!(),
         pk: todo!(),
     };
