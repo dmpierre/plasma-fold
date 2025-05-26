50649c9f680fc548c51d41df3f33610d533e3990
dmpierre <pdaixmoreux@gmail.com>
2025-05-26 14:28:46 +0200

    feat: signer map/tree now consists of public keys as leaves

--- plasma-fold/src/datastructures/signerlist/constraints.rs
+++ plasma-fold/src/datastructures/signerlist/constraints.rs
@@ -3,23 +3,33 @@ use ark_crypto_primitives::{
     merkle_tree::{constraints::ConfigGadget, IdentityDigestConverter},
     sponge::Absorb,
 };
+use ark_ec::CurveGroup;
 use ark_ff::PrimeField;
-use ark_r1cs_std::fields::fp::FpVar;
+use ark_r1cs_std::{fields::fp::FpVar, groups::CurveVar};
 use std::marker::PhantomData;
 
-use crate::{datastructures::user::UserIdVar, primitives::crh::constraints::UserIdVarCRH};
+use crate::{
+    datastructures::keypair::constraints::PublicKeyVar,
+    primitives::crh::constraints::PublicKeyVarCRH,
+};
 
 use super::SignerTreeConfig;
 
-pub struct SignerTreeConfigGadget<F: PrimeField> {
+pub struct SignerTreeConfigGadget<F: PrimeField, C: CurveGroup, CVar: CurveVar<C, F>> {
     _f: PhantomData<F>,
+    _c: PhantomData<C>,
+    _cvar: PhantomData<CVar>,
 }
 
-impl<F: PrimeField + Absorb> ConfigGadget<SignerTreeConfig<F>, F> for SignerTreeConfigGadget<F> {
-    type Leaf = [UserIdVar<F>];
-    type LeafDigest = FpVar<F>;
-    type LeafInnerConverter = IdentityDigestConverter<FpVar<F>>;
-    type InnerDigest = FpVar<F>;
-    type LeafHash = UserIdVarCRH<F>;
-    type TwoToOneHash = TwoToOneCRHGadget<F>;
+impl<F: PrimeField, C: CurveGroup<BaseField = F>, CVar: CurveVar<C, F>>
+    ConfigGadget<SignerTreeConfig<C>, C::BaseField> for SignerTreeConfigGadget<F, C, CVar>
+where
+    C::BaseField: Absorb,
+{
+    type Leaf = PublicKeyVar<C, CVar>;
+    type LeafDigest = FpVar<C::BaseField>;
+    type LeafInnerConverter = IdentityDigestConverter<FpVar<C::BaseField>>;
+    type InnerDigest = FpVar<C::BaseField>;
+    type LeafHash = PublicKeyVarCRH<C, CVar>;
+    type TwoToOneHash = TwoToOneCRHGadget<C::BaseField>;
 }
--- plasma-fold/src/datastructures/signerlist/mod.rs
+++ plasma-fold/src/datastructures/signerlist/mod.rs
@@ -1,54 +1,33 @@
 use std::marker::PhantomData;
 
-use crate::{datastructures::user::UserId, primitives::crh::UserIdCRH};
+use crate::{
+    datastructures::user::UserId,
+    primitives::crh::{PublicKeyCRH, UserIdCRH},
+};
 use ark_crypto_primitives::{
     crh::poseidon::TwoToOneCRH,
     merkle_tree::{Config, IdentityDigestConverter, MerkleTree},
     sponge::Absorb,
 };
+use ark_ec::CurveGroup;
 use ark_ff::PrimeField;
+
+use super::keypair::PublicKey;
 
 pub mod constraints;
 
-pub type SignerList = Vec<UserId>;
+pub type SignerList = Vec<u32>;
 pub type SignerTree<P: Config> = MerkleTree<P>;
 
-pub struct SignerTreeConfig<F: PrimeField> {
-    _f: PhantomData<F>,
+pub struct SignerTreeConfig<C: CurveGroup> {
+    _c: PhantomData<C>,
 }
 
-impl<F: PrimeField + Absorb> Config for SignerTreeConfig<F> {
-    type Leaf = [UserId];
-    type LeafDigest = F;
-    type LeafInnerDigestConverter = IdentityDigestConverter<F>;
-    type InnerDigest = F;
-    type LeafHash = UserIdCRH<F>;
-    type TwoToOneHash = TwoToOneCRH<F>;
-}
-
-#[cfg(test)]
-pub mod tests {
-    use ark_bn254::Fr;
-    use ark_relations::r1cs::ConstraintSystem;
-    use ark_std::rand::thread_rng;
-    use folding_schemes::transcript::poseidon::poseidon_canonical_config;
-
-    use crate::datastructures::user::UserId;
-
-    use super::{SignerTree, SignerTreeConfig};
-
-    #[test]
-    pub fn test_signer_tree_constraints() {
-        let n_users = (2 as usize).pow(10);
-        let mut rng = thread_rng();
-        let pp = poseidon_canonical_config::<Fr>();
-        let cs = ConstraintSystem::<Fr>::new_ref();
-        let user_ids = (0..n_users)
-            .map(|id| [id as u32; 1])
-            .collect::<Vec<[UserId; 1]>>();
-        let signer_tree = SignerTree::<SignerTreeConfig<Fr>>::new(&pp, &pp, user_ids);
-
-        // classic mt, there isn't much to test
-        assert!(signer_tree.is_ok());
-    }
+impl<C: CurveGroup<BaseField: Absorb + PrimeField>> Config for SignerTreeConfig<C> {
+    type Leaf = PublicKey<C>;
+    type LeafDigest = C::BaseField;
+    type LeafInnerDigestConverter = IdentityDigestConverter<C::BaseField>;
+    type InnerDigest = C::BaseField;
+    type LeafHash = PublicKeyCRH<C>;
+    type TwoToOneHash = TwoToOneCRH<C::BaseField>;
 }
