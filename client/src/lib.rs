637b941e661862ce3cf23a3c55b587c57e3702bd
dmpierre <pdaixmoreux@gmail.com>
2025-06-09 17:50:15 +0200

    test: test client circuit size with sha

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,9 +1,16 @@
 use std::marker::PhantomData;
 
 use ark_crypto_primitives::{
-    crh::poseidon::{
-        constraints::{CRHParametersVar, TwoToOneCRHGadget},
-        TwoToOneCRH,
+    crh::{
+        poseidon::{
+            constraints::{CRHParametersVar, TwoToOneCRHGadget},
+            TwoToOneCRH,
+        },
+        sha256::{
+            constraints::{Sha256Gadget, UnitVar},
+            Sha256,
+        },
+        TwoToOneCRHSchemeGadget,
     },
     sponge::{poseidon::PoseidonConfig, Absorb},
 };
@@ -13,7 +20,9 @@ use ark_r1cs_std::{alloc::AllocVar, fields::fp::FpVar, groups::CurveVar};
 use ark_relations::r1cs::{ConstraintSystemRef, SynthesisError};
 use circuits::{UserAux, UserAuxVar, UserCircuit};
 use folding_schemes::{frontend::FCircuit, Error};
-use plasma_fold::primitives::accumulator::constraints::PoseidonAccumulatorVar;
+use plasma_fold::primitives::accumulator::constraints::{
+    PoseidonAccumulatorVar, Sha256AccumulatorVar,
+};
 mod utils;
 
 pub mod circuits;
@@ -72,6 +81,64 @@ impl<
             PoseidonAccumulatorVar<F>,
             N_TX_PER_FOLD_STEP,
         >::new(pp.clone(), pp.clone());
+
+        user_circuit.update_balance(cs, z_i, external_inputs)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ClientCircuitSha<
+    F: PrimeField,
+    C: CurveGroup,
+    CVar: CurveVar<C, F>,
+    const N_TX_PER_FOLD_STEP: usize,
+> {
+    _f: PhantomData<F>,
+    _c: PhantomData<C>,
+    _cvar: PhantomData<CVar>,
+    params: PoseidonConfig<F>,
+}
+
+impl<
+        F: PrimeField + Absorb,
+        C: CurveGroup<BaseField = F>,
+        CVar: CurveVar<C, F>,
+        const N_TX_PER_FOLD_STEP: usize,
+    > FCircuit<F> for ClientCircuitSha<F, C, CVar, N_TX_PER_FOLD_STEP>
+{
+    type Params = PoseidonConfig<F>;
+    type ExternalInputs = UserAux<F, C, N_TX_PER_FOLD_STEP>;
+    type ExternalInputsVar = UserAuxVar<F, C, CVar>;
+
+    fn new(params: Self::Params) -> Result<Self, Error> {
+        Ok(Self {
+            _f: PhantomData::<F>,
+            _c: PhantomData::<C>,
+            _cvar: PhantomData::<CVar>,
+            params,
+        })
+    }
+    fn state_len(&self) -> usize {
+        7
+    }
+    /// generates the constraints for the step of F for the given z_i
+    fn generate_step_constraints(
+        &self,
+        cs: ConstraintSystemRef<F>,
+        _i: usize,
+        z_i: Vec<FpVar<F>>,
+        external_inputs: Self::ExternalInputsVar,
+    ) -> Result<Vec<FpVar<F>>, SynthesisError> {
+        let pp = CRHParametersVar::new_constant(cs.clone(), self.params.clone())?;
+        let user_circuit = UserCircuit::<
+            F,
+            C,
+            CVar,
+            Sha256,
+            Sha256Gadget<F>,
+            Sha256AccumulatorVar<F>,
+            N_TX_PER_FOLD_STEP,
+        >::new(UnitVar::new_constant(cs.clone(), ())?, pp.clone());
 
         user_circuit.update_balance(cs, z_i, external_inputs)
     }
--- client/tests/mod.rs
+++ client/tests/mod.rs
@@ -8,7 +8,7 @@ pub mod tests {
     use ark_relations::r1cs::{ConstraintSystemRef, SynthesisError};
     use ark_serialize::{CanonicalSerialize, Compress};
     use ark_std::rand::thread_rng;
-    use client::ClientCircuitPoseidon;
+    use client::{ClientCircuitPoseidon, ClientCircuitSha};
     use folding_schemes::arith::Arith;
     use folding_schemes::folding::nova::{ProverParams, VerifierParams};
     use folding_schemes::{
@@ -85,6 +85,44 @@ pub mod tests {
 
         println!(
             "Batch size: {}, total circuit size: {}, params size: {}",
+            TEST_BATCH_SIZE,
+            vp.r1cs.n_constraints(),
+            pp_size + vp_size
+        );
+    }
+
+    #[test]
+    pub fn test_print_serialized_params_sha() {
+        pub const TEST_BATCH_SIZE: usize = 2;
+        let pp = poseidon_canonical_config();
+        let mut rng = thread_rng();
+
+        let f_circuit =
+            ClientCircuitSha::<Fr, Projective2, GVar, TEST_BATCH_SIZE>::new(pp.clone()).unwrap();
+
+        let nova_preprocess_params = PreprocessorParam::new(pp.clone(), f_circuit.clone());
+        let (pp, vp): (
+            ProverParams<Projective, Projective2, Pedersen<Projective>, Pedersen<Projective2>>,
+            VerifierParams<Projective, Projective2, Pedersen<Projective>, Pedersen<Projective2>>,
+        ) = Nova::<
+            Projective,
+            Projective2,
+            ClientCircuitSha<Fr, Projective2, GVar, TEST_BATCH_SIZE>,
+            Pedersen<Projective>,
+            Pedersen<Projective2>,
+            false,
+        >::preprocess(&mut rng, &nova_preprocess_params)
+        .unwrap();
+
+        let pp_size =
+            pp.serialized_size(Compress::Yes) + pp.poseidon_config.serialized_size(Compress::Yes);
+
+        let vp_size = vp.serialized_size(Compress::Yes)
+            + vp.r1cs.serialized_size(Compress::Yes)
+            + vp.cf_r1cs.serialized_size(Compress::Yes);
+
+        println!(
+            "[SHA] Batch size: {}, total circuit size: {}, params size: {}",
             TEST_BATCH_SIZE,
             vp.r1cs.n_constraints(),
             pp_size + vp_size
