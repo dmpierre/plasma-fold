699967c4118eb4d03351c617597096f5c3915d84
dmpierre <pdaixmoreux@gmail.com>
2025-06-25 12:59:08 +0100

    chore: remove cubic circuit from test

--- client/tests/mod.rs
+++ client/tests/mod.rs
@@ -1,52 +1,18 @@
 #[cfg(test)]
 pub mod tests {
     use ark_bn254::Fr;
-    use ark_ff::PrimeField;
     use ark_grumpkin::constraints::GVar;
-    use ark_r1cs_std::alloc::AllocVar;
-    use ark_r1cs_std::fields::fp::FpVar;
-    use ark_relations::r1cs::{ConstraintSystemRef, SynthesisError};
     use ark_serialize::{CanonicalSerialize, Compress};
     use ark_std::rand::thread_rng;
     use client::{ClientCircuitPoseidon, ClientCircuitSha};
     use folding_schemes::arith::Arith;
     use folding_schemes::folding::nova::{ProverParams, VerifierParams};
+    use folding_schemes::FoldingScheme;
     use folding_schemes::{
         commitment::pedersen::Pedersen, frontend::FCircuit,
         transcript::poseidon::poseidon_canonical_config,
     };
-    use folding_schemes::{Error, Field, FoldingScheme};
-    use std::marker::PhantomData;
 
-    #[derive(Clone, Copy, Debug)]
-    pub struct CubicFCircuit<F: PrimeField> {
-        _f: PhantomData<F>,
-    }
-
-    impl<F: Field> FCircuit<F> for CubicFCircuit<F> {
-        type Params = ();
-        type ExternalInputs = ();
-        type ExternalInputsVar = ();
-
-        fn new(_params: Self::Params) -> Result<Self, Error> {
-            Ok(Self { _f: PhantomData })
-        }
-        fn state_len(&self) -> usize {
-            1
-        }
-        fn generate_step_constraints(
-            &self,
-            cs: ConstraintSystemRef<F>,
-            _i: usize,
-            z_i: Vec<FpVar<F>>,
-            _external_inputs: Self::ExternalInputsVar,
-        ) -> Result<Vec<FpVar<F>>, SynthesisError> {
-            let five = FpVar::<F>::new_constant(cs.clone(), F::from(5u32))?;
-            let z_i = z_i[0].clone();
-
-            Ok(vec![&z_i * &z_i * &z_i + &z_i + &five])
-        }
-    }
     use ark_bn254::G1Projective as Projective;
     use ark_grumpkin::Projective as Projective2;
 
@@ -87,7 +53,10 @@ pub mod tests {
             "Batch size: {}, total circuit size: {}, params size: {} {} {} {}",
             TEST_BATCH_SIZE,
             vp.r1cs.n_constraints(),
-            pp_size, vp.serialized_size(Compress::Yes), vp.r1cs.serialized_size(Compress::Yes), vp.cf_r1cs.serialized_size(Compress::Yes)
+            pp_size,
+            vp.serialized_size(Compress::Yes),
+            vp.r1cs.serialized_size(Compress::Yes),
+            vp.cf_r1cs.serialized_size(Compress::Yes)
         );
     }
 
@@ -125,7 +94,10 @@ pub mod tests {
             "Batch size: {}, total circuit size: {}, params size: {} {} {} {}",
             TEST_BATCH_SIZE,
             vp.r1cs.n_constraints(),
-            pp_size, vp.serialized_size(Compress::Yes), vp.r1cs.serialized_size(Compress::Yes), vp.cf_r1cs.serialized_size(Compress::Yes)
+            pp_size,
+            vp.serialized_size(Compress::Yes),
+            vp.r1cs.serialized_size(Compress::Yes),
+            vp.cf_r1cs.serialized_size(Compress::Yes)
         );
     }
 }
